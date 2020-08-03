6eda00ab19c1d012c5b79ab3b7fe22b3bd87321d
saffran <yuwensaf@gmail.com>
2020-08-03 18:01:15 +0800

    hw4.js 修改

--- homeworks/week4/hw4.js
+++ homeworks/week4/hw4.js
@@ -1,26 +1,29 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable quote-props */
+/* eslint-disable camelcase */
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-template */
 const request = require('request');
 
+const baseUrl = 'https://api.twitch.tv/kraken';
+const Client_ID = 'ovn0u5ph1z8z2gngt8si4nogad7znx';
+
 const options = {
-  url: 'https://api.twitch.tv/kraken/games/top',
+  url: `${baseUrl}/games/top`,
   headers: {
     'Accept': 'application/vnd.twitchtv.v5+json',
-    'Client-ID': 'ovn0u5ph1z8z2gngt8si4nogad7znx',
+    'Client-ID': Client_ID,
   },
 };
 
 const callback = (error, response, body) => {
-  let obj;
-  try {
-    obj = JSON.parse(body);
-  } catch (err) {
-    console.log(err);
+  if (error) {
+    return console.log('抓取失敗', error);
   }
-
+  const obj = JSON.parse(body);
   const data = obj.top;
   for (let i = 0; i < data.length; i++) {
-    console.log(data[i].viewers, data[i].game.name);
+    console.log(data[i].viewers + ' ' + data[i].game.name);
   }
 };
 
